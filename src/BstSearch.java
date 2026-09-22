b1875d8a0f17c306b6a9f83734321e7938962a96
Samuel <174657033+SamuelM007@users.noreply.github.com>
2026-09-22 11:53:18 -0700

    BstSearch and first test attempt

--- src/BstSearch.java
+++ src/BstSearch.java
@@ -23,6 +23,18 @@ public class BstSearch {
      */
     public static <T extends Comparable<T>> boolean contains(BinaryTreeNode<T> root, T target) {
         // Implement here, and make sure to implement tests too!
+        if(root == null)return false;
+        
+        if(target.compareTo(root.data) == 0) return true;
+
+        if(target.compareTo(root.data) < 0){
+            return contains(root.left,target);
+        }
+
+        if(target.compareTo(root.data) > 0){
+            return contains(root.right,target);
+        }
+
         return false;
     }
 }
--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -3,4 +3,13 @@ import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
 
+    @Test
+    void NullIntTreeToContainsReturnFalse() {
+        BinaryTreeNode<Integer> intTree = new BinaryTreeNode<Integer>(null);
+
+        int target = 10;
+
+        assertEquals(BstSearch.contains(intTree,target), false);
+    }
+
 }
